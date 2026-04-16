33e6b14f92cf9240b98c96c9da671360544292a0
edgard512 <jm.digitalwrd@gmail.com>
2026-04-15 20:21:42 -0500

    Set default Supabase URL in list-doctors.js

--- api/admin/list-doctors.js
+++ api/admin/list-doctors.js
@@ -6,7 +6,7 @@ module.exports = async function handler(req, res) {
   const ADMIN_SECRET = process.env.ADMIN_SECRET || 'admin1234';
 
   const supabase = createClient(
-    process.env.SUPABASE_URL,
+    process.env.SUPABASE_URL || 'https://kvnezyatdmdsmdfkdhyw.supabase.co',
     process.env.SUPABASE_SERVICE_KEY
   );
 
